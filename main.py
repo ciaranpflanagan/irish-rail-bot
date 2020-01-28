cbd1a1f7aa709d87a336f227dd73b7d95636ea7a
Ciaran Flanagan <ciaran@cicisoft.net>
2020-01-28 22:35:22 +0000

    Moved code to function and improved readout and %

--- main.py
+++ main.py
@@ -15,19 +15,22 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
-# Get User
-target = 'IrishRail'
-keywords = ['delays','delay', 'mechanical problem', 'emergency services', 'currently stopped', 'cancelled', 'behind schedule']
-tweet_no = 0
-reply = 0
-item = auth_api.get_user(target)
-for status in Cursor(auth_api.user_timeline, id=target).items(10000):
-	if any(substring in status.text.lower() for substring in keywords):
+def get_percentage(target_account, number_tweets):
+	# Getting User
+	keywords = ['delays','delay', 'mechanical problem', 'emergency services', 'currently stopped', 'canceled', 'behind schedule', 'transferred', 'alternative service', 'fault', 'bus transfers', 'issue', 'disruptions', 'being held', 'service suspended', 'late', 'stopped']
+	tweet_found = 0
+	reply = 0
+	item = auth_api.get_user(target_account)
+	for status in Cursor(auth_api.user_timeline, id=target_account).items(number_tweets):
+		# Filtering out replies to only include tweets
 		if (status.in_reply_to_status_id is None):
-			print(status.text + '\n')
-			tweet_no += 1
+			# If one of the keywords is found, increment counter
+			if any(substring in status.text.lower() for substring in keywords):
+				tweet_found += 1
+			else:
+				print(status.text + '\n')
 		else:
-			# Tweet is a reply
 			reply += 1
-print("Percentage % = ")
-print(tweet_no / (10000 - reply))
+	print("Tweets containing delays = " + str((tweet_found / (number_tweets - reply)) * 100) + "%")
+
+get_percentage('IrishRail', 1000)
